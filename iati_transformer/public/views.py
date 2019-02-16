7805da85ac7c3140121d1049dc890e9795e03b0f
Andy Lulham <a.lulham@gmail.com>
2019-02-16 22:14:52 +0000

    Rejig routes

--- iati_transformer/public/views.py
+++ iati_transformer/public/views.py
@@ -12,8 +12,13 @@ blueprint = Blueprint('public', __name__,  # pylint: disable=invalid-name
                       static_folder='../static')
 
 
-@blueprint.route('/', methods=['GET', 'POST'])
+@blueprint.route('/')
 def home():
+    return render_template('public/home.html')
+
+
+@blueprint.route('/paste', methods=['GET', 'POST'])
+def paste():
     """Show the home page."""
     if request.method == 'POST':
         form_data = request.form
@@ -21,13 +26,13 @@ def home():
         form_data = request.args
     source = form_data.get('source')
     if not source:
-        return render_template('public/home.html')
+        return render_template('public/paste.html')
 
     source_data = BytesIO(source.encode())
     response = validate(source_data)
     flash_msg_type = 'success' if response['success'] else 'danger'
     flash(response['flash_msg'], flash_msg_type)
-    return render_template('public/home.html',
+    return render_template('public/paste.html',
                            source=source,
                            result=response['result'])
 
@@ -47,6 +52,6 @@ def transform():
 
     flash_msg_type = 'success' if response['success'] else 'danger'
     flash(response['flash_msg'], flash_msg_type)
-    return render_template('public/home.html',
+    return render_template('public/paste.html',
                            source=source,
                            result=response['result'])
