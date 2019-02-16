4bfe4a03405541b508504d72ee523f45da283945
Andy Lulham <a.lulham@gmail.com>
2019-02-16 10:45:58 +0000

    Refactor; add a /convert route

---
+++ iati_transformer/public/utils.py
@@ -0,0 +1,67 @@
+from io import BytesIO
+
+from os.path import abspath, dirname, join
+import re
+
+import iatikit
+from lxml import etree
+
+
+basedir = abspath(dirname(__file__))  # pylint: disable=invalid-name
+
+
+def validate(source):
+    flash_msg = None
+    result = ''
+    dataset = iatikit.Dataset(source)
+    if not dataset.validate_xml():
+        flash_msg = 'Input data is not valid XML.'
+    elif not dataset.version.startswith('1'):
+        flash_msg = 'Input data is not IATI v1.0x.'
+    elif not dataset.validate_iati():
+        flash_msg = 'Input data is not valid to the IATI schema.'
+    elif not dataset.validate_codelists():
+        flash_msg = 'Input data uses invalid codelist values.'
+
+    if flash_msg:
+        return {
+            'success': False,
+            'flash_msg': flash_msg,
+            'result': result,
+        }
+
+    xsd_path = join(basedir, '..', 'static', 'iati-activities.xsl')
+    transform = etree.XSLT(etree.parse(xsd_path))
+    transformed = transform(dataset.etree)
+    result = str(transformed)
+    result_data = BytesIO(bytes(transformed))
+    dataset = iatikit.Dataset(result_data)
+    if not dataset.validate_xml():
+        flash_msg = 'There was a problem! Result data not valid XML.'
+    elif not dataset.validate_iati():
+        print(dataset.validate_iati().errors[0].details)
+        flash_msg = 'The data was transformed, but has schema validation issues'
+        match_re = r'the order of elements is important\. .*? but ' + \
+                   r'(.*?) is expected\.$'
+        match = re.search(match_re, dataset.validate_iati().errors[0].details)
+        if match:
+            flash_msg += ' (e.g. an activity is missing {}.)'.format(
+                match.group(1))
+        else:
+            flash_msg += '.'
+    elif not dataset.validate_codelists():
+        print(dataset.validate_codelists().errors[0].details)
+        flash_msg = 'The data was transformed, but has codelist issues.'
+
+    if flash_msg:
+        return {
+            'success': False,
+            'flash_msg': flash_msg,
+            'result': result,
+        }
+
+    return {
+        'success': True,
+        'flash_msg': 'Data successfully transformed!',
+        'result': result,
+    }
--- iati_transformer/public/views.py
+++ iati_transformer/public/views.py
@@ -1,14 +1,13 @@
 """Public section, including homepage and signup."""
-from os.path import abspath, dirname, join
+
 from io import BytesIO
-import re
 
-from flask import Blueprint, render_template, request, flash
-import iatikit
-from lxml import etree
+from flask import Blueprint, render_template, request, flash, Response
+import requests
+
+from .utils import validate
 
 
-basedir = abspath(dirname(__file__))  # pylint: disable=invalid-name
 blueprint = Blueprint('public', __name__,  # pylint: disable=invalid-name
                       static_folder='../static')
 
@@ -24,51 +23,30 @@ def home():
     if not source:
         return render_template('public/home.html')
 
-    error_msg = None
-    result = ''
     source_data = BytesIO(source.encode())
-    dataset = iatikit.Dataset(source_data)
-    if not dataset.validate_xml():
-        error_msg = 'Input data is not valid XML.'
-    elif not dataset.version.startswith('1'):
-        error_msg = 'Input data is not IATI v1.0x.'
-    elif not dataset.validate_iati():
-        error_msg = 'Input data is not valid to the IATI schema.'
-    elif not dataset.validate_codelists():
-        error_msg = 'Input data uses invalid codelist values.'
-
-    if error_msg:
-        flash(error_msg, 'danger')
-        return render_template('public/home.html',
-                               source=source)
-
-    xsd_path = join(basedir, '..', 'static', 'iati-activities.xsl')
-    transform = etree.XSLT(etree.parse(xsd_path))
-    transformed = transform(dataset.etree)
-    result = str(transformed)
-    result_data = BytesIO(bytes(transformed))
-    dataset = iatikit.Dataset(result_data)
-    if not dataset.validate_xml():
-        error_msg = 'There was a problem! Result data not valid XML.'
-    elif not dataset.validate_iati():
-        error_msg = 'The data was transformed, but has schema validation issues'
-        match_re = r'the order of elements is important\. .*? but ' + \
-                   r'(.*?) is expected\.$'
-        match = re.search(match_re, dataset.validate_iati().errors[0].details)
-        if match:
-            error_msg += ' (e.g. an activity is missing {}.)'.format(
-                match.group(1))
-        else:
-            error_msg += '.'
-    elif not dataset.validate_codelists():
-        error_msg = 'The data was transformed, but has codelist issues.'
-
-    if error_msg:
-        flash(error_msg, 'danger')
-        return render_template('public/home.html',
-                               source=source, result=result)
-
-    flash('Data successfully transformed!', 'success')
+    response = validate(source_data)
+    flash_msg_type = 'success' if response['success'] else 'danger'
+    flash(response['flash_msg'], flash_msg_type)
     return render_template('public/home.html',
                            source=source,
-                           result=result)
+                           result=response['result'])
+
+
+@blueprint.route('/convert')
+def convert():
+    url = request.args.get('url')
+    r = requests.get(url)
+    source = r.content.decode()
+    source_data = BytesIO(r.content)
+    response = validate(source_data)
+    if response['success']:
+        return Response(response['result'], mimetype='text/xml')
+
+    if request.args.get('fallback') == 'true':
+        return Response(source, mimetype='text/xml')
+
+    flash_msg_type = 'success' if response['success'] else 'danger'
+    flash(response['flash_msg'], flash_msg_type)
+    return render_template('public/home.html',
+                           source=source,
+                           result=response['result'])
